bc4991ed0f5be443c1e27ef27b8c1abfc1980296
Matt Rieger <mrieger@mrskin.com>
2015-02-03 16:56:45 -0600

    generator creates controller file

---
+++ lib/generators/acts_as_crud/acts_as_crud_generator.rb
@@ -0,0 +1,9 @@
+class ActsAsCrudGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+  argument :model_name, required: true,
+           desc: 'the name of the model the controller you are'\
+           ' creating is controlling'
+  def create_controller_file
+    create_file "app/controllers/#{model_name}s_controller.rb"
+  end
+end
